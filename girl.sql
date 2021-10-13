b11a16e1c48705e7b2f4280e4c33a46c0f3a5069
qyg <1600314850@qq.com>
2021-10-13 21:40:17 +0800

    add: 连接查询

--- girl.sql
+++ girl.sql
@@ -52,18 +52,18 @@ CREATE TABLE `beauty`  (
 -- ----------------------------
 -- Records of beauty
 -- ----------------------------
-INSERT INTO `beauty` VALUES (1, '柳岩', '女', '1988-02-03 00:00:00', '18209876577', NULL, 8);
-INSERT INTO `beauty` VALUES (2, '苍老师', '女', '1987-12-30 00:00:00', '18219876577', NULL, 9);
-INSERT INTO `beauty` VALUES (3, 'Angelababy', '女', '1989-02-03 00:00:00', '18209876567', NULL, 3);
-INSERT INTO `beauty` VALUES (4, '热巴', '女', '1993-02-03 00:00:00', '18209876579', NULL, 2);
-INSERT INTO `beauty` VALUES (5, '周冬雨', '女', '1992-02-03 00:00:00', '18209179577', NULL, 9);
-INSERT INTO `beauty` VALUES (6, '周芷若', '女', '1988-02-03 00:00:00', '18209876577', NULL, 1);
-INSERT INTO `beauty` VALUES (7, '岳灵珊', '女', '1987-12-30 00:00:00', '18219876577', NULL, 9);
-INSERT INTO `beauty` VALUES (8, '小昭', '女', '1989-02-03 00:00:00', '18209876567', NULL, 1);
-INSERT INTO `beauty` VALUES (9, '双儿', '女', '1993-02-03 00:00:00', '18209876579', NULL, 9);
-INSERT INTO `beauty` VALUES (10, '王语嫣', '女', '1992-02-03 00:00:00', '18209179577', NULL, 4);
-INSERT INTO `beauty` VALUES (11, '夏雪', '女', '1993-02-03 00:00:00', '18209876579', NULL, 9);
-INSERT INTO `beauty` VALUES (12, '赵敏', '女', '1992-02-03 00:00:00', '18209179577', NULL, 1);
+INSERT INTO `beauty` VALUES (1, 'liuyan', 'g', '1988-02-03 00:00:00', '18209876577', NULL, 8);
+INSERT INTO `beauty` VALUES (2, 'canglaoshi', 'g', '1987-12-30 00:00:00', '18219876577', NULL, 9);
+INSERT INTO `beauty` VALUES (3, 'Angelababy', 'g', '1989-02-03 00:00:00', '18209876567', NULL, 3);
+INSERT INTO `beauty` VALUES (4, 'reba', 'g', '1993-02-03 00:00:00', '18209876579', NULL, 2);
+INSERT INTO `beauty` VALUES (5, 'zhoudongyu', 'g', '1992-02-03 00:00:00', '18209179577', NULL, 9);
+INSERT INTO `beauty` VALUES (6, 'zhouzhiruo', 'g', '1988-02-03 00:00:00', '18209876577', NULL, 1);
+INSERT INTO `beauty` VALUES (7, 'yuelingshan', 'g', '1987-12-30 00:00:00', '18219876577', NULL, 9);
+INSERT INTO `beauty` VALUES (8, 'xiaozhao', 'g', '1989-02-03 00:00:00', '18209876567', NULL, 1);
+INSERT INTO `beauty` VALUES (9, 'shaunger', 'g', '1993-02-03 00:00:00', '18209876579', NULL, 9);
+INSERT INTO `beauty` VALUES (10, 'wangyuyan', 'g', '1992-02-03 00:00:00', '18209179577', NULL, 4);
+INSERT INTO `beauty` VALUES (11, 'xiaxue', 'g', '1993-02-03 00:00:00', '18209876579', NULL, 9);
+INSERT INTO `beauty` VALUES (12, 'zhaomin', 'g', '1992-02-03 00:00:00', '18209179577', NULL, 1);
 
 -- ----------------------------
 -- Table structure for boys
@@ -79,10 +79,10 @@ CREATE TABLE `boys`  (
 -- ----------------------------
 -- Records of boys
 -- ----------------------------
-INSERT INTO `boys` VALUES (1, '张无忌', 100);
-INSERT INTO `boys` VALUES (2, '鹿晗', 800);
-INSERT INTO `boys` VALUES (3, '黄晓', 50);
-INSERT INTO `boys` VALUES (4, '段誉', 300);
+INSERT INTO `boys` VALUES (1, 'zhangwuji', 100);
+INSERT INTO `boys` VALUES (2, 'luhan', 800);
+INSERT INTO `boys` VALUES (3, 'huangxiaoming', 50);
+INSERT INTO `boys` VALUES (4, 'duanyu', 300);
 
 SET FOREIGN_KEY_CHECKS = 1;
 
